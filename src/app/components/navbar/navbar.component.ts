b8b6d47d815a61b02be6f1bda17fe3ab17511d6c
Carlos Pérez <carlospmoncada@gmail.com>
2022-07-15 01:12:42 -0600

    Header, general content, benefits,protocols sections sketch desing done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'component-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css','../../../styles.css']
 })
 export class NavbarComponent implements OnInit {
-
+  navigation_links = [
+    {
+      name: 'About Us',
+      path: '/',
+    },
+    {
+      name: 'Microdosis',
+      path: '/',
+    },
+    {
+      name: 'Tratamiento',
+      path: '/',
+    },
+    {
+      name: 'Blog',
+      path: '/',
+    },
+    {
+      name: 'Mi perfil',
+      path: '/',
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
