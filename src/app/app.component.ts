9c1b931e4c6b415a13ec07f7979a87338a98143e
Carlos Pérez <carlospmoncada@gmail.com>
2022-07-15 17:57:15 -0600

    Formula, Footer, and end of content section added, with lacks mobile desing principally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,76 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bhavani_Test';
+  footer_links= [
+    {
+      top_link_name: "Acerca de",
+      top_link_path: "/",
+      sub_links: 
+        [
+          {
+            name: "Mi perfil",
+            path: "/",
+          },
+          {
+            name: "Acerca de",
+            path: "/",
+          },
+          {
+            name: "Blog",
+            path: "/",
+          },
+          {
+            name: "Nuestras investigaciones",
+            path: "/",
+          },
+        ]
+    },
+    {
+      top_link_name: "La Microdosis",
+      top_link_path: "/",
+      sub_links: 
+        [
+          {
+            name: "Beneficio",
+            path: "/",
+          },
+          {
+            name: "Fórmulas",
+            path: "/",
+          },
+          {
+            name: "Espectro de Luz",
+            path: "/",
+          },
+          {
+            name: "Protocolo",
+            path: "/",
+          },
+        ]
+    },
+    {
+      top_link_name: "El Tratamiento",
+      top_link_path: "/",
+      sub_links: 
+        [
+          {
+            name: "Tratamiento",
+            path: "/",
+          },
+          {
+            name: "Coaches",
+            path: "/",
+          },
+          {
+            name: "Terapia",
+            path: "/",
+          },
+          {
+            name: "Preguntas Frecuentes",
+            path: "/",
+          },
+        ]
+    },
+  
+  ];
 }
